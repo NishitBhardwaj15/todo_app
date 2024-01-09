b7381f5cde64e7594515fd2a59aaee9ba1946c2b
Nishit Bhardwaj <nishitjrt@gmail.com>
2024-01-09 21:17:57 +0530

    home structure complete

--- lib/screens/Home/home.dart
+++ lib/screens/Home/home.dart
@@ -12,7 +12,23 @@ class Home extends StatefulWidget{
 class _Home extends State<Home>{
   @override
   Widget build(BuildContext context) {
-    return const Text("Hellow");
+    return Scaffold( 
+      backgroundColor: Colors.amber.shade100,
+      appBar: AppBar( 
+        title: const Center( 
+          child: Text("Tasks"),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.yellow
+      ),
+      body: const Text("body"),
+      floatingActionButton: FloatingActionButton( 
+        onPressed: (){},
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.yellow,
+        child: const Icon(Icons.add),
+      ),
+    );
   }
   
 }
