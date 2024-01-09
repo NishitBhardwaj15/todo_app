ab9e42672e3bbd5896305c487934c88d69c2a808
Nishit Bhardwaj <nishitjrt@gmail.com>
2024-01-09 20:54:29 +0530

    initial files and structure updated

---
+++ lib/screens/Home/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget{
+  const Home({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _Home();
+  }
+}
+
+class _Home extends State<Home>{
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hellow");
+  }
+  
+}
