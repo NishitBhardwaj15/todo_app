5a7ca92d2d476769a1d539980fafdbb046ebed98
Nishit Bhardwaj <nishitjrt@gmail.com>
2024-01-10 00:36:54 +0530

    dummy data for model  added

---
+++ lib/data/home_data.dart
@@ -0,0 +1,6 @@
+import 'package:todo_app/screens/home/model/home_model.dart';
+
+final tasks = [ 
+  HomeModel(title: "Shoping", date: DateTime.now()),
+  HomeModel(title: "workout", date: DateTime.now())
+];
