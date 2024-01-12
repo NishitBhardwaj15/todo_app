4fef7f9f1951f6de9206b627038a5ca7aa094158
Nishit Bhardwaj <nishitjrt@gmail.com>
2024-01-12 23:30:10 +0530

    home model changed

--- lib/screens/Home/model/home_model.dart
+++ lib/screens/Home/model/home_model.dart
@@ -1,21 +1,15 @@
 import 'package:uuid/uuid.dart';
-import 'package:intl/intl.dart';
 
 const uuid = Uuid();
-final formatter = DateFormat().add_yMd();
 
 class HomeModel {
   final String id;
   final String title;
-  final DateTime date;
   bool isSelected = false;
 
   HomeModel({ 
     required this.title,
-    required this.date
   }) : id = uuid.v4();
 
-  String get formattedDate{
-    return formatter.format(date);
-  }
+  
 }
