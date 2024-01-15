d3d78fcd852a1f8f8cf0e0268ea3f1a17f2e3dd2
Nishit Bhardwaj <nishitjrt@gmail.com>
2024-01-15 09:43:21 +0530

    hive dependencies added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:todo_app/screens/home/home.dart';
 
 void main(){
+  
   runApp( 
     MaterialApp( 
       debugShowCheckedModeBanner: false,
