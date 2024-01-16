6ba554428bbad9eda1c6e1bf9c125f58ed555ed2
Nishit Bhardwaj <nishitjrt@gmail.com>
2024-01-16 09:45:45 +0530

    Revert "hive dependencies added"
    This reverts commit d3d78fcd852a1f8f8cf0e0268ea3f1a17f2e3dd2.

--- lib/screens/Home/model/home_model.dart
+++ lib/screens/Home/model/home_model.dart
@@ -3,10 +3,11 @@ import 'package:uuid/uuid.dart';
 const uuid = Uuid();
 
 class HomeModel {
+  final String id;
   final String title;
   bool isSelected = false;
 
   HomeModel({ 
     required this.title,
-  });
+  }) : id = uuid.v4();
 }
