6954f668303b3fd6ac798a3d497437ff55fb8331
Nishit Bhardwaj <nishitjrt@gmail.com>
2024-01-16 13:03:29 +0530

    hive initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/screens/home/home.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main(){
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  await Hive.openBox("taskBox");
+
   runApp( 
     MaterialApp( 
       debugShowCheckedModeBanner: false,
